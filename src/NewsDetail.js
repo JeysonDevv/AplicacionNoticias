d8bdd89b9c1edb0a9d5a4d30360015def4992251
JeysonDevv <jeysongabriel.jimenez@gmail.com>
2023-06-15 19:09:22 -0600

    Paso 7: Personalización del estilo de los componentes NewsDetail y NewsList

--- src/NewsDetail.js
+++ src/NewsDetail.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import {useRoute} from '@react-navigation/native';
+import { View, Text, StyleSheet, Image } from "react-native";
+import { useRoute } from "@react-navigation/native";
 
 const NewsDetail = ({ navigation }) => {
-    const route=useRoute();
-    const { newsItem } = route.params?.newsItem;
+  const route = useRoute();
+  const newsItem = route.params?.newsItem;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{newsItem.title}</Text>
-      <Text style={styles.description}>{newsItem.description}</Text>
+      {newsItem.image && (
+        <Image source={{ uri: newsItem.image }} style={styles.image} />
+      )}
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>{newsItem.title}</Text>
+        <Text style={styles.description}>{newsItem.description}</Text>
+      </View>
     </View>
   );
 };
@@ -17,14 +22,28 @@ const NewsDetail = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    resizeMode: "cover",
+  },
+  contentContainer: {
     padding: 16,
   },
   title: {
-    fontSize: 18,
+    fontSize: 24,
     fontWeight: "bold",
+    marginBottom: 8,
+    textAlign: "justify",
+    color:"blue",
   },
   description: {
-    fontSize: 14,
+    fontSize: 16,
+    color: "#555",
+    textAlign: "justify",
   },
 });
 
--- src/NewsList.js
+++ src/NewsList.js
@@ -1,63 +1,91 @@
-import React, {useEffect, useState} from "react";
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Image } from "react-native";
 import axios from "axios";
 
-const NewsList = ({navigation})=>{
-    const [news, setNews] = useState([]);
+const NewsList = ({ navigation }) => {
+  const [news, setNews] = useState([]);
 
-    useEffect(()=>{
-        fetchNews();
-    }, []);
+  useEffect(() => {
+    fetchNews();
+  }, []);
 
-    const fetchNews = async()=>{
-        try{
-            const response = await axios.get('https://newsapi.org/v2/top-headlines', {
-                params:{
-                    country:'us',
-                    apiKey: '87e2d12910e6415caeba669689069349'
-                },
-            });
-            setNews(response.data.articles);
-            }catch(error){
-                console.error(error);
-            }
-        };
-
-        return (
-            <View style={styles.container}>
-                <FlatList
-                    data={news}
-                    renderItem={({item})=>(
-                        <TouchableOpacity
-                            style={styles.newsItem}
-                            key={item.id}
-                            onPress={()=>navigation.navigate('NewsDetail', {newsItem:item})}
-                        >
-                            <Text style={styles.title}>{item.title}</Text>
-                            <Text style={styles.description}>{item.description}</Text>
-                        </TouchableOpacity>
-                    )}
-                    keyExtractor={(item)=>item.id?.toString()}
-                />
-            </View>
-        );
-    };
-
-    const styles=StyleSheet.create({
-        container:{
-            flex:1,
-            padding:16,
+  const fetchNews = async () => {
+    try {
+      const response = await axios.get("https://newsapi.org/v2/top-headlines", {
+        params: {
+          country: "us",
+          apiKey: "87e2d12910e6415caeba669689069349",
         },
-        newsItem:{
-            marginBottom:16,
-        },
-        title:{
-            fontSize:18,
-            fontWeight:'bold',
-        },
-        description:{
-            fontSize:14,
-        },
-    });
+      });
+      setNews(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    export default NewsList;
+  const renderNewsItem = ({ item }) => {
+    return (
+      <TouchableOpacity
+        style={styles.newsItem}
+        onPress={() => navigation.navigate("NewsDetail", { newsItem: item })}
+      >
+        <Image source={{ uri: item.urlToImage }} style={styles.image} />
+        <View style={styles.content}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.description}>{item.description}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={news}
+        renderItem={renderNewsItem}
+        keyExtractor={(item) => item.id?.toString()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  newsItem: {
+    flexDirection: "row",
+    marginBottom: 16,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    overflow: "hidden",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  image: {
+    width: 120,
+    height: 120,
+  },
+  content: {
+    flex: 1,
+    padding: 12,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  description: {
+    fontSize: 14,
+    color: "#888",
+  },
+});
+
+export default NewsList;
